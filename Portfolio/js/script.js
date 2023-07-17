850bb3a6a75f5dcb5bbae547e8bfa1a4351fb39c
BhojrajCSE21 <bhojrajnarware@gmail.com>
2023-07-17 20:12:08 +0530

    Did the first two task of internship

---
+++ Portfolio/js/script.js
@@ -0,0 +1,9 @@
+/* Typing animation */
+var typed = new Typed(".typing", {
+    strings: ["","Web Developer", "Web Designer", "Machine Learning Enthusiast", "Python Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,  
+    loop: true
+    
+
+});
